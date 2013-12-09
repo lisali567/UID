247753793c7f343943b8e700ad9f3c78537a27f0
Lisa Li <ll2768@columbia.edu>
2013-12-08 20:21:32 -0500

    Added new pages and saved trip functionality

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var listArr=new Array();
 var listLat=new Array();
 var listLng=new Array();
-window.requestFileSystem(window.TEMPORARY, 1024 * 1024, onInitFileSystem, errorHandler);
 var searchFail="failed";
 var searchLat=0;
 var searchLng=0;
+var listNames=[];
+var uniqueArr;
+var myListArr=[];
 
 function createNewItin()
 {
@@ -14,14 +16,6 @@ function createNewItin()
 function hideItin()
 {
 	$("#newItin").hide();
-}
-
-function saveItin()
-{
-	$("#newItin").hide();
-	$("#searchResults").hide();
-	$("#finalList").show();	
-	viewMap();
 }
 
 function onInitFileSystem(filesystem) {
@@ -104,19 +98,33 @@ function addPlaceToList(venueName, venueValue)
 {
 	$("#add"+venueValue+"").css("background-color", "green").attr("value","Added!");
 	listArr.push(venueName);
-	var uniqueArr=listArr.filter(function(itm,i,listArr){
+	uniqueArr=listArr.filter(function(itm,i,listArr){
     	return i==listArr.indexOf(itm);
 	});
 	$('#addedL').html('');
+	$('#itinDestList').html('');
 	$.each(uniqueArr, function( index, value ) {
+		console.log("the index: "+index+" the value: "+value+" the venueName: "+venueName)
 		$("#addedL").append('<option value='+index+'>'+value+'</option>');
+		$("#itinDestList").append('<p>'+value+'<button class="moreInfo">More Information</button><button class="remove">X Remove</button></p><hr/>');
 	});
 	console.log("the selected is "+ venueName);
+	var temp1;
+	var temp2;
 	$.getJSON('https://api.foursquare.com/v2/venues/'+ venueValue+'?oauth_token=JHPVGF1KAR2F5RBE0JDHKU0X0KOVGQGZKPFFJDNL3QIDMIH1&v=20131201',
     function(data) {
 		listLat.push(data.response.venue.location.lat);
     	listLng.push(data.response.venue.location.lng);
+    	temp1=data.response.venue.location.lat;
+    	temp2=data.response.venue.location.lat;
     });
+    myListArr.push(
+	{
+		id:venueValue,
+		name:venueName,
+		lat: temp1,
+		lng: temp2
+	});
 }
 
 function viewMap()
@@ -157,4 +165,29 @@ function viewMap()
 		//console.log('the feature is '+ features[i].properties.title);
 		}
 	}
+}
+
+//TODO: save the array listArr to local storage with listName captured below
+function saveList()
+{
+	var listName=$('#listName').val();
+	listNames.push(listName);
+	store.set(listName,myListArr);
+	$("#newItin").hide();
+	$("#searchResults").hide();
+	$("#finaListName").html('');
+	$("#finaListName").append("<em>"+listName+"</em> Itinerary");
+	$("#finalList").show();	
+	viewMap();
+}
+
+
+function displayAllListNames()
+{
+	var ctr=0;
+	store.forEach(function(key,val) {
+	    console.log(key);
+	    $("#myTrips").append('<input type="checkbox"><p style="display:inline;">'+key+'</input><button class="edit">View/Edit</button></p><hr/>');
+	    ctr++;
+	});
 }
